a36b1baee110923de6f44b0f2282d238a9195ce6
pwngithub <110849398+pwngithub@users.noreply.github.com>
2025-10-03 16:15:02 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,20 @@
+# constants.py
+
+STATUS_ORDER = ["Open", "In Progress", "Escalated", "On Hold", "Resolved", "Closed"]
+PRIORITY_ORDER = ["Low", "Medium", "High", "Critical"]
+
+STATUS_COLOR = {
+    "Open": "blue",
+    "In Progress": "purple",
+    "Escalated": "red",
+    "On Hold": "orange",
+    "Resolved": "green",
+    "Closed": "gray",
+}
+
+PRIORITY_COLOR = {
+    "Low": "gray",
+    "Medium": "blue",
+    "High": "orange",
+    "Critical": "red",
+}
